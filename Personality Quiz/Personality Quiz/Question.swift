68f249b2fb83b373e0cd4ec9eb81fe3d2a81dddc
Seerat0003 <seerat4811.be23@chitkara.edu.in>
2025-09-17 10:28:47 +0530

    Add files via upload

---
+++ Personality Quiz/Personality Quiz/Question.swift
@@ -0,0 +1,42 @@
+//
+//  Question.swift
+//  Personality Quiz
+//
+//  Created by Student on 29/07/25.
+//
+
+import Foundation
+struct Question{
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+enum ResponseType{
+    case single, multiple, ranged
+}
+
+struct Answer{
+    var text: String
+    var type: AnimalType
+}
+
+enum AnimalType: Character {
+    case lion = "🦁", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String{
+        switch self{
+        case .lion:
+            return "You're incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+            
+        case .cat:
+            return "Mischevious, yet mild-tempered, you enjoy doing things on your own terms."
+        
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+            
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race." 
+        }
+    }
+}
